2ba737c9da9e7eb6bbb58aac2cd4a27e6633fb5e
Ren Komatsu <komatsu@robot.t.u-tokyo.ac.jp>
2020-09-29 18:09:23 +0900

    Add example to rectify fisheye images

--- example/image_rectification.py
+++ example/image_rectification.py
@@ -1,5 +1,17 @@
+import cv2
+
 from dscamera import DSCamera
 
 if __name__ == "__main__":
     json_file = "./calibration.json"
     cam = DSCamera(json_file)
+    img = cv2.imread("./sample.jpg")
+    perspective = cam.to_perspective(img)
+    equirect = cam.to_equirect(img)
+
+    # Display
+    cv2.imshow("orig", img)
+    cv2.imshow("perspective", perspective)
+    cv2.imshow("equirect", equirect)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
